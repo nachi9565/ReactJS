4da13f83aa02176d735b7e02b14d268691b4bd44
Nachiappa Palaniyappa <p.nachi95@gmail.com>
2021-07-21 14:00:07 -0500

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Select from 'react-select';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Button } from 'bootstrap';
 // import data from './databricks_instances_config.json'
 // var config = {}
 
@@ -12,6 +13,10 @@ class App extends React.Component {
       // project_name_list: [],
       // instance_types_list: [],
       // app_name_list: [],
+      project_name = '',
+      instance_type = '',
+      app_name = '',
+      branch_name = '',
       project_names: [],
       instance_types: [],
       app_names: [],
@@ -39,7 +44,8 @@ class App extends React.Component {
 
   onProjectSelect = (e) => {
     // console.log(e['value']);
-    let instance_type_list = ['dev', 'prod'];
+    // let instance_type_list = ['dev', 'prod'];
+    this.setState({instance_type: e['value']});
     // this.setState({instance_types: this.gererate_dropdown_data(instance_type_list)})
     fetch("https://api.example.com/items")
     .then(res => res.json())
@@ -56,7 +62,8 @@ class App extends React.Component {
   }
 
   onInstanceSelect = (e) => {
-    let app_name_list = ['BIT', 'FIS'];
+    // let app_name_list = ['BIT', 'FIS'];
+    this.setState({app_name: e['value']});
     fetch("https://api.example.com/items")
     .then(res => res.json())
     .then( 
@@ -72,7 +79,8 @@ class App extends React.Component {
   }
 
   onAppSelect = (e) => {
-    let branch_name_list = ['master', 'dev'];
+    // let branch_name_list = ['master', 'dev'];
+    this.setState({branch_name: e['value']});
     fetch("https://api.example.com/items")
     .then(res => res.json())
     .then( 
@@ -94,37 +102,53 @@ class App extends React.Component {
     return dropdown_data;
   }
 
+  onCompareClick = (e) => {
+    fetch("url",
+    { 
+        method: "POST",
+        headers: { "Content-Type": "application/json",'Authorization': 'Bearer ' + window.localStorage["Access_Token"]},
+        body:data
+    }).then(response => response.blob()).then(response => ...*your code for download*... )
+  }
+
   render() {
-  return( <div className="container">
-    <div className="row" style={{padding:'5px'}}>
-      <div className="col-md-4"></div>
-      <div className="col-md-4">
-        <Select options={ this.state.project_names } onChange={this.onProjectSelect}/>
+    return( <div className="container">
+      <div className="row" style={{padding:'5px'}}>
+        {/* <div className="col-md-4"></div> */}
+        <div className="col-md-4">
+          <Select options={ this.state.project_names } onChange={this.onProjectSelect}/>
+        </div>
+        {/* <div className="col-md-4"></div> */}
       </div>
-      <div className="col-md-4"></div>
-    </div>
-    <div className="row" style={{padding:'5px'}}>
-      <div className="col-md-4"></div>
-      <div className="col-md-4">
-        <Select options={ this.state.instance_types } onChange={this.onInstanceSelect} isDisabled={this.state.isInstanceDisabled}/>
+      <div className="row" style={{padding:'5px'}}>
+        {/* <div className="col-md-4"></div> */}
+        <div className="col-md-4">
+          <Select options={ this.state.instance_types } onChange={this.onInstanceSelect} isDisabled={this.state.isInstanceDisabled}/>
+        </div>
+        {/* <div className="col-md-4"></div> */}
       </div>
-      <div className="col-md-4"></div>
-    </div>
-    <div className="row" style={{padding:'5px'}}>
-      <div className="col-md-4"></div>
-      <div className="col-md-4">
-        <Select options={ this.state.app_names } onChange={this.onAppSelect} isDisabled={this.state.isAppDisabled}/>
+      <div className="row" style={{padding:'5px'}}>
+        {/* <div className="col-md-4"></div> */}
+        <div className="col-md-4">
+          <Select options={ this.state.app_names } onChange={this.onAppSelect} isDisabled={this.state.isAppDisabled}/>
+        </div>
+        {/* <div className="col-md-4"></div> */}
       </div>
-      <div className="col-md-4"></div>
-    </div>
-    <div className="row" style={{padding:'5px'}}>
-      <div className="col-md-4"></div>
-      <div className="col-md-4">
-        <Select options={ this.state.branch_names } isDisabled={this.state.isBranchDisabled}/>
+      <div className="row" style={{padding:'5px'}}>
+        {/* <div className="col-md-4"></div> */}
+        <div className="col-md-4">
+          <Select options={ this.state.branch_names } isDisabled={this.state.isBranchDisabled}/>
+        </div>
+        {/* <div className="col-md-4"></div> */}
       </div>
-      <div className="col-md-4"></div>
-    </div>
-  </div> );
+      <div className="row" style={{padding:'5px'}}>
+        {/* <div className="col-md-4"></div> */}
+          <div className="col-md-4">
+            <button onClick={this.onCompareClick}>Compare</button>
+          </div>
+        {/* <div className="col-md-4"></div> */}
+      </div>
+    </div> );
   }
 }
 
